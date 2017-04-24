b222ff694bfbd2b4bc273cbf8a738b7d2fd39a26
Yannick Laevaert <yannicklaevaert@telenet.be>
2017-04-24 18:14:36 +0200

    Manual maze

---
+++ Manual Maze/Assets/Scripts/MazeDoor.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class MazeDoor : MonoBehaviour {
+
+	public Transform hinge;
+
+
+	void OnTriggerEnter (Collider other) {
+		if (other.gameObject.tag == "Player") {
+			OpenDoor (other);
+		}
+//		if (other.gameObject.tag == "Player") {
+//			Debug.Log (gameObject.transform.rotation.y);
+//			if (gameObject.transform.rotation == Quaternion.identity) {
+//				if (other.gameObject.transform.position.z > gameObject.transform.position.z + 0.5) {
+//					hinge.localRotation = Quaternion.Euler (0f, 90f, 0f);
+//				} else if (other.gameObject.transform.position.z < gameObject.transform.position.z + 0.5) {
+//					hinge.localRotation = Quaternion.Euler (0f, -90f, 0f);
+//				}
+//			}
+//			else if (gameObject.transform.rotation == Quaternion.Euler (0f, 90f, 0f)) {
+//				Debug.Log ("Test");
+//				if (other.gameObject.transform.position.x > gameObject.transform.position.x + 0.5) {
+//					hinge.localRotation = Quaternion.Euler (0f, 90f, 0f);
+//				} else if (other.gameObject.transform.position.x < gameObject.transform.position.x + 0.5) {
+//					hinge.localRotation = Quaternion.Euler (0f, -90f, 0f);
+//				}
+//			}
+//			else if (gameObject.transform.rotation == Quaternion.Euler (0f, 180f, 0f)) {
+//				if (other.gameObject.transform.position.z < gameObject.transform.position.z - 0.5) {
+//					hinge.localRotation = Quaternion.Euler (0f, 90f, 0f);
+//				} else if (other.gameObject.transform.position.z > gameObject.transform.position.z - 0.5) {
+//					hinge.localRotation = Quaternion.Euler (0f, -90f, 0f);
+//				}
+//			}
+//			else if (gameObject.transform.rotation == Quaternion.Euler (0f, 270f, 0f)) {
+//				if (other.gameObject.transform.position.x < gameObject.transform.position.x - 0.5) {
+//					hinge.localRotation = Quaternion.Euler (0f, 90f, 0f);
+//				} else if (other.gameObject.transform.position.x > gameObject.transform.position.x - 0.5) {
+//					hinge.localRotation = Quaternion.Euler (0f, -90f, 0f);
+//				}
+//			}
+//		}
+	}
+
+	void OnTriggerExit (Collider other) {
+		if (other.gameObject.tag == "Player") {
+			hinge.localRotation = Quaternion.identity;
+		}
+	}
+
+	protected void OpenDoor(Collider other) {
+		if (this.transform.rotation == Quaternion.identity) {
+			if (other.gameObject.transform.position.z > this.transform.position.z + 0.5) {
+				hinge.localRotation = Quaternion.Euler (0f, 90f, 0f);
+			} else if (other.gameObject.transform.position.z < this.transform.position.z + 0.5) {
+				hinge.localRotation = Quaternion.Euler (0f, -90f, 0f);
+			}
+		}
+		else if (this.transform.rotation == Quaternion.Euler (0f, 90f, 0f)) {
+			Debug.Log ("Test");
+			if (other.gameObject.transform.position.x > this.transform.position.x + 0.5) {
+				hinge.localRotation = Quaternion.Euler (0f, 90f, 0f);
+			} else if (other.gameObject.transform.position.x < this.transform.position.x + 0.5) {
+				hinge.localRotation = Quaternion.Euler (0f, -90f, 0f);
+			}
+		}
+		else if (this.transform.rotation == Quaternion.Euler (0f, 180f, 0f)) {
+			if (other.gameObject.transform.position.z < this.transform.position.z - 0.5) {
+				hinge.localRotation = Quaternion.Euler (0f, 90f, 0f);
+			} else if (other.gameObject.transform.position.z > this.transform.position.z - 0.5) {
+				hinge.localRotation = Quaternion.Euler (0f, -90f, 0f);
+			}
+		}
+		else if (this.transform.rotation == Quaternion.Euler (0f, 270f, 0f)) {
+			if (other.gameObject.transform.position.x < this.transform.position.x - 0.5) {
+				hinge.localRotation = Quaternion.Euler (0f, 90f, 0f);
+			} else if (other.gameObject.transform.position.x > this.transform.position.x - 0.5) {
+				hinge.localRotation = Quaternion.Euler (0f, -90f, 0f);
+			}
+		}
+	}
+}
---
+++ Manual Maze/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float moveSpeed = 5.0f; 
+
+	void Start() {
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveX = Input.GetAxis ("Horizontal");
+		float moveZ = Input.GetAxis ("Vertical");
+
+
+		moveX *= (moveSpeed * Time.deltaTime);
+		moveZ *= (moveSpeed * Time.deltaTime);
+		// Move
+		//Vector3 movement = new Vector3(moveX, 0f, moveZ);
+		//GetComponent<Rigidbody> ().velocity = movement * moveSpeed * Time.deltaTime;
+		transform.Translate(moveX, 0, moveZ);
+
+		if (Input.GetKeyDown ("escape")) {
+			Cursor.lockState = CursorLockMode.None;
+		}
+	}
+}
