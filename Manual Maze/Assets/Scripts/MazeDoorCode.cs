692fd6748d4098d2f75b4d40db3897a0afcdb7d7
Eline <eline.beslic@hotmail.com>
2017-05-01 15:51:53 +0200

    deuren af

--- Manual Maze/Assets/Scripts/MazeDoorCode.cs
+++ Manual Maze/Assets/Scripts/MazeDoorCode.cs
@@ -10,17 +10,16 @@ public class MazeDoorCode : MonoBehaviour {
 	public string input;
 	public bool onTrigger;
 	public bool doorOpen;
-	public bool keypadScreen;
+	public bool doorClosed;
 	public Collider other;
 	public Transform hinge;
+	public string message = "Authorized personnel only.";
 
 
 	void OnTriggerEnter (Collider other) {
 		this.other = other;
 		onTrigger = true;
-		/*if (other.gameObject.tag == "Player") {
-			OpenDoor (other);
-		}*/
+		doorClosed = false;
 	}
 
 	void OnTriggerExit (Collider other) {
@@ -29,8 +28,8 @@ public class MazeDoorCode : MonoBehaviour {
 		}*/
 		this.other = other;
 		onTrigger = false;
-		keypadScreen = false;
 		input = "";
+		doorClosed = true;
 	}
 
 	protected void OpenDoor() {
@@ -66,14 +65,20 @@ public class MazeDoorCode : MonoBehaviour {
 	}
 
 	void Update() {
-		if(input == curPassword){
+		/*if(input == curPassword){
 			doorOpen = true;
-		}
+		}*/
 
 		if(doorOpen){
 			/*var newRot = Quaternion.RotateTowards(hinge.rotation, Quaternion.Euler(0.0f, -90.0f, 0.0f), Time.deltaTime * 250);
 			hinge.rotation = newRot;*/
 			OpenDoor();
+		}
+
+		if (doorClosed){
+			if (other.gameObject.tag == "Player") {
+			hinge.localRotation = Quaternion.identity;
+			}
 		}
 	}
 
@@ -84,60 +89,80 @@ public class MazeDoorCode : MonoBehaviour {
 			{
 				
 				//GUI.Box(new Rect(0, 0, 220, 330), "");
-				GUI.Box(new Rect(100, 100, 200, 25), "Enter code to open door:");
-				GUI.Box(new Rect(100, 125, 200, 25), input);
+				GUI.Box(new Rect(100, 100, 200, 25), message);
+				GUI.Box(new Rect(100, 125, 200, 25), "Enter code to open door:");
+				GUI.Box(new Rect(100, 150, 200, 25), input);
 
 				if(Event.current.Equals(Event.KeyboardEvent(KeyCode.Keypad1.ToString()))){
+					message = "Authorized personnel only.";
 					input = input + "1";
 				}
 
 				if(Event.current.Equals(Event.KeyboardEvent(KeyCode.Keypad2.ToString()))){
+					message = "Authorized personnel only.";
 					input = input + "2";
 				}
 
 				if(Event.current.Equals(Event.KeyboardEvent(KeyCode.Keypad3.ToString())))
 				{
+					message = "Authorized personnel only.";
 					input = input + "3";
 				}
 
 				if(Event.current.Equals(Event.KeyboardEvent(KeyCode.Keypad4.ToString())))
 				{
+					message = "Authorized personnel only.";
 					input = input + "4";
 				}
 
 				if(Event.current.Equals(Event.KeyboardEvent(KeyCode.Keypad5.ToString())))
 				{
+					message = "Authorized personnel only.";
 					input = input + "5";
 				}
 
 				if(Event.current.Equals(Event.KeyboardEvent(KeyCode.Keypad6.ToString())))
 				{
+					message = "Authorized personnel only.";
 					input = input + "6";
 				}
 
 				if(Event.current.Equals(Event.KeyboardEvent(KeyCode.Keypad7.ToString())))
 				{
+					message = "Authorized personnel only.";
 					input = input + "7";
 				}
 
 				if(Event.current.Equals(Event.KeyboardEvent(KeyCode.Keypad8.ToString())))
 				{
+					message = "Authorized personnel only.";
 					input = input + "8";
 				}
 
 				if(Event.current.Equals(Event.KeyboardEvent(KeyCode.Keypad9.ToString())))
 				{
+					message = "Authorized personnel only.";
 					input = input + "9";
 				}
 
 				if(Input.GetKeyDown(KeyCode.Keypad0))
 				{
+					message = "Authorized personnel only.";
 					input = input + "0";
 				}
 
 				if(Event.current.Equals(Event.KeyboardEvent(KeyCode.Backspace.ToString())))
 				{
+					message = "Authorized personnel only.";
 					input = input.Substring(0, input.Length - 1);
+				}
+			}
+			if (input.Length == 4){
+				if(input == curPassword){
+					doorOpen = true;
+				} else {
+					message = "Wrong code, please try again.";
+					input = "";
 				}
 			}
 		}
