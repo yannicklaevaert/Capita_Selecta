fae474ed2fcf0aa946d6cdc78708dfaa7a0c1997
martijn <martijn.millecamp@student.kuleuven.be>
2017-05-07 17:57:40 +0200

    fix tijd

--- Manual Maze/Assets/Scripts/MazeDoorSlideLocked.cs
+++ Manual Maze/Assets/Scripts/MazeDoorSlideLocked.cs
@@ -6,12 +6,15 @@ public class MazeDoorSlideLocked : MonoBehaviour {
 	Animator animator;
 	bool doorOpen;
 	public string curPassword = "123456";
+	public static string neededColor = "Green";
+	public static string neededType = "A";
 	public string input;
 	bool onTrigger;
 	bool doorLocked;
 	bool doorClosed;
 	Collider other;
-	public string message = "Authorized personnel only.";
+	public string message = 
+		"Authorized personnel only. You need a " + neededType + " key of type " + neededType ;
 
 	void Start()
 	{
@@ -74,66 +77,66 @@ public class MazeDoorSlideLocked : MonoBehaviour {
 				GUI.Box(new Rect(100, 150, 200, 25), input);
 
 				if(Event.current.Equals(Event.KeyboardEvent(KeyCode.Keypad1.ToString()))){
-					message = "Authorized personnel only.";
+					message = "Authorized personnel only. You need a " + neededType + " key of type " + neededType ;
 					input = input + "1";
 				}
 
 				if(Event.current.Equals(Event.KeyboardEvent(KeyCode.Keypad2.ToString()))){
-					message = "Authorized personnel only.";
+					message = "Authorized personnel only. You need a " + neededType + " key of type " + neededType ;
 					input = input + "2";
 				}
 
 				if(Event.current.Equals(Event.KeyboardEvent(KeyCode.Keypad3.ToString())))
 				{
-					message = "Authorized personnel only.";
+					message = "Authorized personnel only. You need a " + neededType + " key of type " + neededType ;
 					input = input + "3";
 				}
 
 				if(Event.current.Equals(Event.KeyboardEvent(KeyCode.Keypad4.ToString())))
 				{
-					message = "Authorized personnel only.";
+					message = "Authorized personnel only. You need a " + neededType + " key of type " + neededType ;
 					input = input + "4";
 				}
 
 				if(Event.current.Equals(Event.KeyboardEvent(KeyCode.Keypad5.ToString())))
 				{
-					message = "Authorized personnel only.";
+					message = "Authorized personnel only. You need a " + neededType + " key of type " + neededType ;
 					input = input + "5";
 				}
 
 				if(Event.current.Equals(Event.KeyboardEvent(KeyCode.Keypad6.ToString())))
 				{
-					message = "Authorized personnel only.";
+					message = "Authorized personnel only. You need a " + neededType + " key of type " + neededType ;
 					input = input + "6";
 				}
 
 				if(Event.current.Equals(Event.KeyboardEvent(KeyCode.Keypad7.ToString())))
 				{
-					message = "Authorized personnel only.";
+					message = "Authorized personnel only. You need a " + neededType + " key of type " + neededType ;
 					input = input + "7";
 				}
 
 				if(Event.current.Equals(Event.KeyboardEvent(KeyCode.Keypad8.ToString())))
 				{
-					message = "Authorized personnel only.";
+					message = "Authorized personnel only. You need a " + neededType + " key of type " + neededType ;
 					input = input + "8";
 				}
 
 				if(Event.current.Equals(Event.KeyboardEvent(KeyCode.Keypad9.ToString())))
 				{
-					message = "Authorized personnel only.";
+					message = "Authorized personnel only. You need a " + neededType + " key of type " + neededType ;
 					input = input + "9";
 				}
 
 				if(Input.GetKeyDown(KeyCode.Keypad0))
 				{
-					message = "Authorized personnel only.";
+					message = "Authorized personnel only. You need a " + neededType + " key of type " + neededType ;
 					input = input + "0";
 				}
 
 				if(Event.current.Equals(Event.KeyboardEvent(KeyCode.Backspace.ToString())))
 				{
-					message = "Authorized personnel only.";
+					message = "Authorized personnel only. You need a " + neededType + " key of type " + neededType ;
 					input = input.Substring(0, input.Length - 1);
 				}
 			}
