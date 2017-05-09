d05165e7568a235ba58618f30615d701f2b2c836
Eline <eline.beslic@hotmail.com>
2017-05-09 11:28:55 +0200

    robotje

--- Manual Maze/Assets/Scripts/GameManager.cs
+++ Manual Maze/Assets/Scripts/GameManager.cs
@@ -26,7 +26,6 @@ public class GameManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log(time);
 		time -= Time.deltaTime;
 	    if((time < 0) || enemyCollision){
 	        alive = false;
--- Manual Maze/Assets/Scripts/PathFollower.cs
+++ Manual Maze/Assets/Scripts/PathFollower.cs
@@ -5,21 +5,26 @@ using UnityEngine;
 public class PathFollower : MonoBehaviour {
 
 	public Transform[] path;
+	public Quaternion rot;
 	public float speed = 1.0f;
+	public float turnSpeed = 5.0f;
 	public float reachDist = 0.5f;
 	public int currentPoint = 0;
 
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+	//Variables for sight
+	public float heightMultiplier;
+	public float sightDist = 10;
 	
 	// Update is called once per frame
 	void Update () {
+		//Investigate();
 		Vector3 dir = path[currentPoint].position - transform.position;
 
-		transform.position += dir * Time.deltaTime * speed;		
+		transform.position += dir * Time.deltaTime * speed;	
+
+		rot = Quaternion.LookRotation(dir);
+   		// Slerp to it over time:
+   		transform.rotation = Quaternion.Slerp(transform.rotation, rot, turnSpeed * Time.deltaTime);	
 
 		if (dir.magnitude <= reachDist){
 			currentPoint++;
@@ -40,8 +45,32 @@ public class PathFollower : MonoBehaviour {
 	}
 
 	void OnTriggerEnter (Collider other) {
-		GameManager.enemyCollision = true;
+		if (other.gameObject.tag == "Player") {
+			GameManager.enemyCollision = true;
+		}
 	}
+
+	/*void Investigate(){
+		RaycastHit hit;
+		Debug.DrawRay(transform.position + Vector3.up / heightMultiplier, transform.forward * sightDist, Color.green);
+		Debug.DrawRay(transform.position + Vector3.up / heightMultiplier, (transform.forward + transform.right).normalized * sightDist, Color.green);
+		Debug.DrawRay(transform.position + Vector3.up / heightMultiplier, (transform.forward - transform.right).normalized * sightDist, Color.green);
+		if (Physics.Raycast(transform.position + Vector3.up / heightMultiplier, transform.forward, out hit, sightDist)){
+			if (hit.collider.gameObject.tag == "Player"){
+				GameManager.enemyCollision = true;
+			}
+		}
+		if (Physics.Raycast(transform.position + Vector3.up / heightMultiplier, (transform.forward + transform.right).normalized, out hit, sightDist)){
+			if (hit.collider.gameObject.tag == "Player"){
+				GameManager.enemyCollision = true;
+			}
+		}
+		if (Physics.Raycast(transform.position + Vector3.up / heightMultiplier, (transform.forward - transform.right).normalized, out hit, sightDist)){
+			if (hit.collider.gameObject.tag == "Player"){
+				GameManager.enemyCollision = true;
+			}
+		}
+	}*/
 
 	/*void OnTriggerExit (Collider other) {
 		/*if (other.gameObject.tag == "Player") {
