b213a6f2315832c3e2aeb6550f9aab2dac47a1d3
Eline <eline.beslic@hotmail.com>
2017-05-12 11:58:53 +0200

    gameover screen

--- Manual Maze/Assets/Scripts/GameManager.cs
+++ Manual Maze/Assets/Scripts/GameManager.cs
@@ -84,5 +84,7 @@ public class GameManager : MonoBehaviour {
 		//AudioSource.PlayClipAtPoint(gameover, transform.position);
 		timeLeft.text = "Game Over";
 		Time.timeScale = 0f;
+		Application.LoadLevel("GameOver");
+
 	}
 }
